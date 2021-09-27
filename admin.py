901f6e98754b9331c825a2515a97595b2d672a97
daniel <daniel@daniel.com>
2021-09-27 17:37:22 +0300

    content_manager Document & RichContent merged in just Document. Fixed bug where CustomMetaTags created with no content throws server error 500.

--- admin.py
+++ admin.py
@@ -1,9 +1,5 @@
 from django.contrib import admin
-from .models import RichContent, Document
-
-
-class RichContentAdmin(admin.ModelAdmin):
-    readonly_fields = ('date_created', 'date_modified')
+from .models import Document
 
 
 class DocumentAdmin(admin.ModelAdmin):
@@ -11,5 +7,4 @@ class DocumentAdmin(admin.ModelAdmin):
 
 
 # Register models:
-admin.site.register(RichContent, RichContentAdmin)
 admin.site.register(Document, DocumentAdmin)
--- models.py
+++ models.py
@@ -4,12 +4,10 @@ from ckeditor.fields import RichTextField
 from django.utils.html import format_html
 
 
-class RichContent(models.Model):
-    title = models.CharField(
-        blank=False, null=False, max_length=150,
-        help_text="Enter an internal title for this content, this is for internal use and not displayed to users."
-    )
-    content = RichTextField()
+class Document(models.Model):
+    main_title = models.CharField(max_length=250, help_text="Main document header, this should be the H1 tag unless "
+                                                            "mentioned otherwise.")
+    content = RichTextField(help_text="Keep empty to display default (if default exists)")
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
 
@@ -18,17 +16,6 @@ class RichContent(models.Model):
             return format_html(self.content)
         else:
             return ""
-
-    def __str__(self):
-        return "%s Content" % self.title
-
-
-class Document(models.Model):
-    main_title = models.CharField(max_length=250, help_text="Main document header, this should be the H1 tag unless "
-                                                            "mentioned otherwise.")
-    content = models.ForeignKey(RichContent, help_text="The main content of this document", on_delete=models.CASCADE)
-    date_created = models.DateTimeField(auto_now_add=True)
-    date_modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return "Document: %s" % self.main_title
